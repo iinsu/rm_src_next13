b05b3981b9fe78e44ea11a7c68314613dd81f900
iinsue <hinsu@rmsoft.kr>
2023-09-03 20:48:17 +0900

    store 생성

---
+++ src/app/todo/store.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+interface TodoProps {
+  id: number;
+  text: string;
+}
+
+interface TodoListProps {
+  todoList: TodoProps[];
+  addTodo: (newTodo: TodoProps) => void;
+  deleteTodo: (todo: TodoProps) => void;
+}
+
+export const useTodoListStore = create<TodoListProps>((set) => ({
+  todoList: [],
+  // TODO 추가
+  addTodo: (newTodo) =>
+    set((props) => ({ todoList: [...props.todoList, newTodo] })),
+
+  // TODO 제거
+  deleteTodo: (todo) =>
+    set((props) => {
+      const filteredList = props.todoList.filter((item) => item.id !== todo.id);
+      return { todoList: filteredList };
+    }),
+}));
