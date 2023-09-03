b4aa26f3556c407f0414fbaed1ae403e3fbe692c
iinsue <hinsu@rmsoft.kr>
2023-09-03 20:18:40 +0900

    todo page 로 이동할 버튼 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <div>Home</div>
-      <Link href="/todo">ToDo</Link>
+      <Link href="/todo">
+        <Button>ToDo</Button>
+      </Link>
     </>
   );
 }
